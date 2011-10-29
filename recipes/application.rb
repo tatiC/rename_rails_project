6e1b5f0a5cb8d1ff9ca57c0886cb42b13e953d85
Tati Carvalho <tatigmc@yahoo.com.br>
2011-10-28 22:33:33 -0200

    Added the code to change the other 10 files

--- recipes/application.rb
+++ recipes/application.rb
@@ -1,3 +1,14 @@
 new_app_name = ask("Name of your new awesome rails project")
 
 gsub_file "app/views/layouts/application.html.erb", "#{app_name}".camelize, new_app_name.camelize
+gsub_file "config/application.rb", "#{app_name}".camelize, new_app_name.camelize
+gsub_file "config/environment.rb", "#{app_name}".camelize, new_app_name.camelize
+gsub_file "config/environments/development.rb", "#{app_name}".camelize, new_app_name.camelize
+gsub_file "config/environments/production.rb", "#{app_name}".camelize, new_app_name.camelize
+gsub_file "config/environments/test.rb", "#{app_name}".camelize, new_app_name.camelize
+gsub_file "config/initializers/secret_token.rb", "#{app_name}".camelize, new_app_name.camelize
+gsub_file "config/initializers/session_store.rb", "#{app_name}".camelize, new_app_name.camelize
+gsub_file "config/initializers/session_store.rb", "_#{app_name}_session", "_#{new_app_name}_session"
+gsub_file "config/routes.rb", "#{app_name}".camelize, new_app_name.camelize
+gsub_file "config.ru", "#{app_name}".camelize, new_app_name.camelize
+gsub_file "Rakefile", "#{app_name}".camelize, new_app_name.camelize
